6432d860e272aac53ee49dc8e01f1faba0bd4ebf
Jonathan Parker <jonathanp62@gmail.com>
2024-07-26 06:27:45 -0400

    Upgraded the version to 0.10.0

--- src/main/java/net/jmp/demo/redis/Version.java
+++ src/main/java/net/jmp/demo/redis/Version.java
@@ -1,6 +1,7 @@
 package net.jmp.demo.redis;
 
 /*
+ * (#)Version.java  0.10.0  07/26/2024
  * (#)Version.java  0.9.0   06/22/2024
  * (#)Version.java  0.8.1   06/14/2024
  * (#)Version.java  0.8.0   06/14/2024
@@ -12,7 +13,7 @@ package net.jmp.demo.redis;
  * (#)Version.java  0.2.0   05/01/2024
  *
  * @author    Jonathan Parker
- * @version   0.9.0
+ * @version   0.10.0
  * @since     0.2.0
  *
  * MIT License
@@ -43,7 +44,7 @@ package net.jmp.demo.redis;
  */
 final class Version {
     /** The version. */
-    final static String VERSION_STRING = "0.9.0";
+    final static String VERSION_STRING = "0.10.0";
 
     /**
      * The default constructor.
